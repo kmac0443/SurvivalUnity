ff5d64e6af2bc4ab2320f403420490385df6b980
Eric Hatch <sirquelthas@yahoo.com>
2015-02-28 20:16:29 -0700

    Wrote a bunch of basic listener delegates for updating the view that other classes can add themselves to

---
+++ Assets/Scripts/Controllers/ViewController/EnvironmentListener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentListener : MonoBehaviour {
+
+	public delegate void EnvironmentChanged();
+	public static event EnvironmentChanged onChanged;
+
+	//When Environment is changed, update the environment
+	void OnEnvironmentChanged() 
+	{
+		if(onChanged != null)
+		{
+			onChanged();
+		}
+		//foreach (resource in environment.ResourceObjects)
+		//	unity.changeimage(resource)
+	}
+}
