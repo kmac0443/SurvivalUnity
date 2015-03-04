e688380bd6b07389cb054479cbf05fd279b4f115
gitNate <nwatterson@gmail.com>
2015-03-03 23:52:38 -0700

    good laws...

---
+++ Assets/Scripts/Controllers/View/SceneListener.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneListener : MonoBehaviour {
+
+	public delegate void sceneChanged();
+	public static event sceneChanged onChanged;
+
+	//When Scene is changed, update scene.
+	void OnSceneChanged()//(SceneModel scene) 
+	{
+		if(onChanged != null)
+		{
+			onChanged();
+		}
+		//Unity.changescene(scene.currentscene)
+	}
+}
---
+++ Assets/Scripts/Controllers/View/TestListeners.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ViewController
+{
+	public class TestListeners : MonoBehaviour 
+	{
+		static void Main(string[] args)
+		{
+			GUIMeterUpd MeterUpd = new GUIMeterUpd ();
+			MeterUpd.onEnable ();
+		}
+	}
+}
