731d1b7c3e01d461d991decd31bd07f07b089fb4
kolipoki <ldstrumpet@gmail.com>
2015-03-10 09:52:52 -0600

    Clicking the button adds an item to the inventory.

--- Assets/Scripts/View/HUD/Test_ModelScript.cs
+++ Assets/Scripts/View/HUD/Test_ModelScript.cs
@@ -9,25 +9,15 @@ public class Test_ModelScript : MonoBehaviour
     void Start()
     {
         inv = new Inventory();
-		for (int i = 0; i < 5; ++i) {
-			inv.AddItem(new Item(0, "hey", Item.Type.Item0));
-			inv.AddItem(new Item(0, "hey", Item.Type.Item1));
-			inv.AddItem(new Item(0, "hey", Item.Type.Item2));
-			inv.AddItem(new Item(0, "hey", Item.Type.Item3));
-		}
+		inv.AddItem(new Item(0, "hey", Item.Type.Item0));
+		inv.AddItem(new Item(0, "hey", Item.Type.Item1));
+		inv.AddItem(new Item(0, "hey", Item.Type.Item2));
+		inv.AddItem(new Item(0, "hey", Item.Type.Item3));
     }
 
     void OnMouseDown()
     {
-		print("Adding item to inventory");
-		print (inv.Items.Count + " num of items in inventory");
-		foreach (Item myItem in inv.Items)
-		{
-			print ("I'm Item: " + myItem.ID);
-		}
-		testingItem = new Item();
-		print("My Item is " + testingItem + " but has an ID of " + testingItem.ID);
-		inv.AddItem(testingItem);
+		inv.AddItem(new Item(0, "hey", Item.Type.Item0));
     }
 
 }
