d2251d697957dd8f2f3a548c61f6f09bd77e17c2
kolipoki <ldstrumpet@gmail.com>
2015-03-13 21:25:19 -0600

    Added a lovely frog npc.  Load house_1 to view.

---
+++ Assets/Scripts/Unity/NPC_Controller_Script.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_Controller_Script : MonoBehaviour {
+
+	bool facingRight = true;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> () as Animator;
+		StartCoroutine ("RandomMovement");
+	}
+
+	private IEnumerator RandomMovement()
+	{
+		while (true) {
+			yield return new WaitForSeconds((float)(Random.Range(2, 5)));
+			int move = Random.Range (0, 4);
+			switch(move)
+			{
+				case 0:
+					animator.SetTrigger("Move_Forward");
+					break;
+				case 1:
+					if(!facingRight) {
+						Flip ();
+					}
+					animator.SetTrigger("Move_Left");
+					break;
+				case 2:
+					if(facingRight){
+						Flip ();
+					}
+					animator.SetTrigger("Move_Right");
+					break;
+					
+				case 3:
+					animator.SetTrigger("Move_Back");
+					break;
+				case 4:
+					Debug.Log ("Whoops");
+					break;
+			}
+		}
+	}
+
+	void Flip() 
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
