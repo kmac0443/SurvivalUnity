52a4b2636932564dd2965a46b1dca9f9ef3971f9
Kevin MacMaster <kmac0443@gmail.com>
2015-03-25 15:34:29 -0600

    Beginning Time Management System

---
+++ Assets/Scripts/Model/Time/TimeManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour {
+
+	public int Time = 1;
+	private const int DAYEND = 24;
+
+	private ArrayList TimeAffectedList = new ArrayList();
+
+	public void AddTimeAffected(ITimeAffected Affected) {
+		TimeAffectedList.Add(Affected);
+	}
+
+	public void AddHour() {
+		AddHours(1);
+	}
+
+	public void AddHours(int hours) {
+		Time += hours;
+		Time = Time > DAYEND ? (Time - DAYEND) : Time;
+
+		foreach (ITimeAffected Affected in TimeAffectedList) 
+			Affected.OnHour();
+	}
+
+}
