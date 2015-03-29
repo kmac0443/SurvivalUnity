95228e8fcbb843924d1954bfc4d6e48596562287
Wyatt Hepler <wyatt.hepler+git@gmail.com>
2015-03-28 19:38:30 -0600

    Added house sprites & prefabs, sample player in main scene

--- Assets/Scripts/Unity/PlayerController.cs
+++ Assets/Scripts/Unity/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /*
  * Controls the player with the keyboard.
@@ -9,12 +10,48 @@ public class PlayerController : MonoBehaviour {
 	public float speed = 1.0f;
 
 	private GameObject collidingWith = null;
+	private HashSet<GameObject> sprites = new HashSet<GameObject>();
+
+	void Start() {
+		GetAllNonPlayerSprites(GameObject.Find("Sprites").transform);
+	}
+
+	/* Get all of the children with renderers */
+	private void GetAllNonPlayerSprites(Transform parent) {
+		foreach (Transform sprite in parent) {
+			if (sprite.GetComponent<Renderer>() != null && sprite.GetComponent<Collider2D>() != null
+			      && sprite.GetComponent<Renderer>().sortingLayerName == "Character" && (sprite != this.transform)) {
+				sprites.Add(sprite.gameObject);
+			}
+
+			GetAllNonPlayerSprites(sprite);
+		}
+	}
+
+	/* Rearrange the rendering order of the sprites */
+	private void reorderSprites() {
+		// this is O(n) in the number of sprites, which shouldn't be a problem
+		foreach (GameObject sprite in sprites) {
+			if (SpriteOrderer.InFrontOf(this.gameObject, sprite)) {
+				if (sprite.GetComponent<Renderer>().sortingOrder > GetComponent<Renderer>().sortingOrder) {
+					// move the sprite behind you
+					sprite.GetComponent<Renderer>().sortingOrder -= GetComponent<Renderer>().sortingOrder+1;
+				}
+			}
+			else if (sprite.GetComponent<Renderer>().sortingOrder < GetComponent<Renderer>().sortingOrder) {
+				// move the sprite in front of you
+				sprite.GetComponent<Renderer>().sortingOrder += GetComponent<Renderer>().sortingOrder+1;
+			}
+		}
+	}
 
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 		
 		GetComponent<Rigidbody2D>().MovePosition(GetComponent<Rigidbody2D>().position + speed * new Vector2(moveHorizontal, moveVertical) * Time.deltaTime);
+
+		reorderSprites();
 	}
 
 	/*
@@ -41,8 +78,4 @@ public class PlayerController : MonoBehaviour {
 
 		collidingWith = null;
 	}
-
-
-
-
 }
