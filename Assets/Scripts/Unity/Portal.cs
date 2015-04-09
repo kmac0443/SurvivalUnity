5635042bfab65f37fd5f527bf615310b3defd58f
Wyatt Hepler <wyatt.hepler+git@gmail.com>
2015-04-08 21:19:09 -0600

    Added "Portal" interactable script for scene transitions

---
+++ Assets/Scripts/Unity/Portal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : Interactable {
+	public string goToScene;
+
+	void Awake() {
+		if (goToScene == null) {
+			Debug.LogError("Portal " + gameObject.name + " has no destination scene!");
+		}
+	}
+
+	public override bool interact(GameObject actor) {
+		Application.LoadLevel(goToScene);
+		return false;
+	}
+}
