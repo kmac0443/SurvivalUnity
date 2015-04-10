7ca4de217a6937042ed82ffe101890db5875bd45
Wyatt Hepler <wyatt.hepler+git@gmail.com>
2015-04-10 11:27:27 -0600

    Added UI to main scene & KeepAlive script
    KeepAlive keeps a unique object alive between scenes and prevents
    duplicates when returning to the scene where it was created. It is used to
    keep the UI up without adding it to every scene.

---
+++ Assets/KeepAlive.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class KeepAlive : MonoBehaviour {
+	static HashSet<string> exists = new HashSet<string>();
+
+	public string uniqueName = null;
+
+	/* Delete the object if it shouldn't exist */	
+	void Awake() {
+		if (exists.Contains(uniqueName)) {
+			Destroy(this.gameObject);
+		}
+		else {
+			DontDestroyOnLoad(gameObject);
+			exists.Add(uniqueName);
+		}
+	}
+
+	/* Set a default name */
+	void OnValidate() {
+		if (uniqueName == null) uniqueName = gameObject.name;
+	}
+}
